f8529b8c7d3e951d988a1fda8f99ff42ce17d5ec
nigelmarongere <nigelmarongere@gmail.com>
2022-10-09 14:22:26 +0100

    workshop 1 complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { FormControl } from 'react-bootstrap';
+import { Box, Container } from '@mui/material';
 
 const LIMIT = 150;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
+  // initialize state
+  const [pokemonRaw, setPokemonRaw] = useState([]);
+  const [pokemonFiltered, setPokemonFiltered] = useState([]);
+
+  // fetch data
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(pokeApi);
+      const data = await response.json();
+      setPokemonRaw(data.results);
+      setPokemonFiltered(data.results);
+    };
+    fetchData();
+  },[]);
+
+  const handleChange = (ev) => {
+    // get input value
+    const value = ev.target.value;
+    // regex to match input value
+    const regex = new RegExp(value, 'gi');
+    // filter matches from pokemonRaw
+    const filtered = pokemonRaw.filter((pokemon) => {
+      return pokemon.name.match(regex);
+    });
+    // set pokemonFiltered to matches
+    setPokemonFiltered(filtered);
+  }
 
   return (
     <div data-testid="app">
       <Navigation />
-
-      <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      <Box display="flex" p={3}>
+        <InputGroup>
+          <InputGroup.Text>Search</InputGroup.Text>
+          <FormControl placeholder="name" onChange={handleChange}/>
+        </InputGroup>
+      </Box>
+      <Container sx={{ display: 'flex', flexWrap: 'wrap' }}>
+        {pokemonFiltered.map(pokemon => <PokemonCard key={pokemon.name} url={pokemon.url} name={pokemon.name}/>)}
+      </Container>
     </div>
   );
 }
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import {useState, useEffect } from 'react';
+import Card from 'react-bootstrap/Card';
 
 function PokemonCard({ url, name }) {
+  const [image, setImage] = useState('');
+  const [abilities, setAbilities] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setImage(data.sprites.front_default);
+      setAbilities(data.abilities);
+    };
+    fetchData();
+  },[]);
+
+  const listItems = abilities.map(obj => <li>{obj.ability.name}</li>);
 
   return (
-    <div>
-        pokemon card
-    </div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image}/>
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Subtitle>Abilities</Card.Subtitle>
+        <Card.Text as="div"><ul>{listItems}</ul></Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
